844c334d38e36fc19917b2afd9cf6ced11dbcf6f
Jeffrey Price <jeff.price@gmail.com>
2023-02-02 15:31:40 -0500

    Strong Password Solution

--- 3month/week5/strong_password.rb
+++ 3month/week5/strong_password.rb
@@ -1,8 +1,28 @@
 # !/bin/ruby
 # frozen_string_literal: true
 
-require 'pry'
+require 'pry-byebug'
 
-def minimum_number(n, password)
+def minimum_number(_, password)
+  needed = %i[needs_special? needs_digit? needs_upper? needs_lower?].reduce(0) do |sum, m|
+    sum + method(m).call(password)
+  end
+
+  [needed, (6 - password.length)].max
 end
 
+def needs_special?(s)
+  s.match?(/[!@\#$%\^&*()\-+]/) ? 0 : 1
+end
+
+def needs_digit?(s)
+  s.match?(/\d/) ? 0 : 1
+end
+
+def needs_lower?(s)
+  s.match?(/[a-z]/) ? 0 : 1
+end
+
+def needs_upper?(s)
+  s.match?(/[A-Z]/) ? 0 : 1
+end
--- spec/3month/week5/strong_password_spec.rb
+++ spec/3month/week5/strong_password_spec.rb
@@ -6,7 +6,24 @@ require_relative '../../../3month/week5/strong_password'
 
 describe '#minimum_number' do
   it 'is a method' do
-
     expect((method :minimum_number).class).to be Method
+  end
+
+  it 'returns 2 for example 1' do
+    expect(minimum_number(5, '2bbbb')).to eq(2)
+  end
+
+  it 'returns 1 for example 2' do
+    expect(minimum_number(5, '2bb#A')).to eq(1)
+  end
+
+  it 'returns 3 for sample test 0' do
+    expect(minimum_number(3, 'Ab1')).to eq(3)
+  end
+
+  it 'handles passwords with a special char in them' do
+    ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'].each do |s|
+      expect(minimum_number(6, "Ab1ab#{s}")).to eq(0)
+    end
   end
 end
