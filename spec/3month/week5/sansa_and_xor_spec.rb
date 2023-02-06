9d142ce3ec8ac1a6e042164995ad5554bbeba7c8
Jeffrey Price <jeff.price@gmail.com>
2023-02-05 21:56:55 -0500

    broken implementation

--- spec/3month/week5/sansa_and_xor_spec.rb
+++ spec/3month/week5/sansa_and_xor_spec.rb
@@ -8,4 +8,12 @@ describe '#sansa_xor' do
   it 'is a method' do
     expect((method :sansa_xor).class).to be Method
   end
+
+  it 'returns 2 for [1,2,3]' do
+    expect(sansa_xor([1, 2, 3])).to eq 2
+  end
+
+  it 'returns 0 for [4,5,7,5]' do
+    expect(sansa_xor([4, 5, 7, 5])).to eq 2
+  end
 end
