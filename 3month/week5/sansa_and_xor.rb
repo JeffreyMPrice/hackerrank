0b2779eccffc1501c477ea17741057d85f430815
Jeffrey Price <jeff.price@gmail.com>
2023-02-06 10:20:04 -0500

    Sansa and XOR solution.

--- 3month/week5/sansa_and_xor.rb
+++ 3month/week5/sansa_and_xor.rb
@@ -4,17 +4,18 @@
 require 'pry-byebug'
 
 def sansa_xor(arr)
-  permutations_arr = []
-  arr.size.times do |i|
-    permutations_arr += arr.each_cons(i + 1).to_a
-  end
+  # a ^ a is always 0
+  # a by itself is always a
+  # using the algorithm defined, a number will always show up an even number of times
+  # if the array length is even, or an odd number of times if odd.
+  # therefore the answer is 0 if the array length is even
+  # otherwise it's an XOR of all the EVEN elements which will appear and odd number of times
+  if arr.length.even?
+    0
+  else
+    xor = 0
+    arr.each_with_index { |v, idx| xor = xor ^ v if idx.even? }
 
-  collector = permutations_arr.map do |pa|
-    if pa.length == 1
-      pa[0]
-    else
-      pa.reduce { |s, x| s.nil? ? x  : s ^ x }
-    end
+    xor
   end
-  collector.reduce { |s, x| s.nil? ? x : s ^ x }
 end
