5fd82721b51700021cd5cd235aca7ea9460e1ddf
Jeffrey Price <jeff.price@gmail.com>
2023-02-05 21:56:49 -0500

    broken implementation

--- 3month/week5/sansa_and_xor.rb
+++ 3month/week5/sansa_and_xor.rb
@@ -3,4 +3,21 @@
 
 require 'pry-byebug'
 
-def sansa_xor(arr); end
+def sansa_xor(arr)
+  permutations_arr = []
+  arr.size.times do |i|
+    permutations_arr += arr.each_cons(i + 1).to_a
+  end
+
+  collector = permutations_arr.map do |pa|
+    if pa.length == 1
+      pa[0]
+    else
+      y = x
+      pa.each do |x|
+      end
+    end
+  end
+  binding.pry
+  collector.map { |s, x| s ^ x }
+end
